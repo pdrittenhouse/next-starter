dc8d6836edbef110ba68ce94b1a02084ce3bde2a
P.D. Rittenhouse <pdee999@gmail.com>
2024-02-26 12:09:48 -0500

    adding global scss variables

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'scss')],
+        prependData: "@import '../scss/variables.scss';",
     },
 };
 
