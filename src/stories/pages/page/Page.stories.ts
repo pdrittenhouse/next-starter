d3b6148f855f98245cef8516f2124f380e066598
P.D. Rittenhouse <pdee999@gmail.com>
2024-02-26 00:33:03 -0500

    adding docs to page component

--- src/stories/pages/page/Page.stories.ts
+++ src/stories/pages/page/Page.stories.ts
@@ -6,6 +6,8 @@ import { Page } from './Page';
 const meta = {
   title: 'Design System/Pages/Page',
   component: Page,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: 'fullscreen',
