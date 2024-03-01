388e202c69cc24dbf67d8ac1bb1911596c2d9053
P.D. Rittenhouse <pdee999@gmail.com>
2024-03-01 18:08:32 -0500

    adding queries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,30 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import { useQuery } from "@apollo/client";
-import GET_TOASTERS from '@/data/queries';
+import {GET_SITE_DATA} from '@/data/site';
 import { Page } from '@/stories/pages/page/Page';
 
 export default function Home() {
 
-  const { loading, error, data } = useQuery(GET_TOASTERS)
+  const { loading, error, data } = useQuery(GET_SITE_DATA)
 
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`
 
   return (
     <>
-      {console.log(data)}
-      {data.toasters.edges.map((toaster, key) => (
-          <div key={key}>
-            <h2>{toaster.node.title}</h2>
-          </div>
-      ))}
-
-      <Page></Page>
+      <Page>
+        {data.generalSettings && (
+            <>
+              {console.log(data.generalSettings)}
+              <h1>Endpoint</h1>
+              <h4>
+                <a href={data.generalSettings.url}>{data.generalSettings.title}</a>
+              </h4>
+              <p>{data.generalSettings.description}</p>
+            </>
+        )}
+      </Page>
     </>
   );
 }
